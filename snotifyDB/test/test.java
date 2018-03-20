5e21a233a70f90766df3f1147394884e653a5b5b
rustax <rustam.stanikzai@gmail.com>
2018-03-20 16:55:42 +0100

    a test connection to the MariaDB

--- snotifyDB/test/test.java
+++ snotifyDB/test/test.java
@@ -1,15 +1,27 @@
 package test;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class test {	
-	public static void main(String[] cmdLn) throws SQLException {
+	public static void main(String[] cmdLn) throws SQLException, FileNotFoundException {
+		Scanner scan = new Scanner(new FileReader("/home/rustax/Documents/dblogin.txt"));
+		String user = scan.nextLine();
+		user = user.substring(user.indexOf(" ")+1);
+		String pw = scan.nextLine();
+		pw = pw.substring(pw.indexOf(" ")+1);
+		String ip = scan.nextLine();
+		ip = ip.substring(ip.indexOf(" ")+1);
+		scan.close();
+		
 		Statement stmt = null;
-		Connection connection = DriverManager.getConnection("jdbc:mariadb://mrjosu9112.synology.me:3307/test?user=root&password=sn0t1fy.123");
+		Connection connection = DriverManager.getConnection("jdbc:mariadb://" + ip + "/test?user=" + user + "&password=" + pw);
 		stmt = connection.createStatement();
 		String sql;
 		sql = "SELECT userID, fName, lName FROM Users";
